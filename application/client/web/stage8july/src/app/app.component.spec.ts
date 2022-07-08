e37cea699622362cce365639ab84aebf16845a2b
gepinfo <info@geppettosoftware.com>
2022-07-08 05:45:46 +0000

    code commited from geppetto!

---
+++ application/client/web/stage8july/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'stage8july'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('stage8july');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('stage8july app is running!');
+  });
+});
---
+++ application/services/default_services/systemcredentialmanager/src/credentials/securecredentials.ts
@@ -0,0 +1,4 @@
+export let credentials:any = {
+    DB_AUTH_stage8july_4352: 'mongodb://admin:password@',
+    DB_COLL_stage8july_4352: ':27017/stage8july_4352?authSource=admin'
+}
